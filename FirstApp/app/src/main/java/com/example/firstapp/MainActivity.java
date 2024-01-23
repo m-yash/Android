fbfc957f9cdd62e9f28218959f1bf6691032fa37
Yash <iamyashmistry@gmail.com>
2024-01-23 18:01:03 -0500

    Initial commit

---
+++ FirstApp/app/src/main/java/com/example/firstapp/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.firstapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void SubmitButton(View view)
+    {
+        Log.i("info", "button clicked");
+    }
+}
